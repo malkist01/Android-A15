81a7771b24bc933013d27d7ba3e2cc39957fcb18
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-02-01 00:59:14 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Cloning dependencies"
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable  clang
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
 git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
-git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/malkist01/anykernel.git AnyKernel
 echo "Done"
 CHATID="-1002287610863"
 API_BOT="7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8"
