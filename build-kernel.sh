d086f696733975a6c54b7b56f649e568a57da508
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-02-21 12:11:55 +0700

    Add files via upload

--- build-kernel.sh
+++ build-kernel.sh
@@ -70,7 +70,7 @@ function error_sticker() {
 }
 function clone_tc() {
 [ -d ${TC_PATH} ] || mkdir ${TC_PATH}
-git clone -b 17 --depth=1 https://gitlab.com/anandhan07/aosp-clang.git ${TC_PATH}
+git clone -b clang-20.0 --depth=1 https://gitlab.com/kutemeikito/rastamod69-clang.git ${TC_PATH}
 PATH="${TC_PATH}/bin:$PATH"
 export COMPILER=$(${TC_PATH}/bin/clang -v 2>&1 | grep ' version ' | sed 's/([^)]*)[[:space:]]//' | sed 's/([^)]*)//')
 }
